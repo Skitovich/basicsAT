559514a8d7a5d687bb7c99a85d8984051cf9c8a3
Skitovich Ruslan <zloycvetok@gmail.com>
2020-10-31 21:11:56 +0300

    first task

--- src/test/java/ru/netology/CashbackHackerTest.java
+++ src/test/java/ru/netology/CashbackHackerTest.java
@@ -1,18 +1,18 @@
 package ru.netology;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
-import static org.testng.Assert.assertEquals;
 
 
-class CashbackHackerTest {
+public class CashbackHackerTest {
 
     @Test
     public void ShouldShowBuyUnderThousand () {
         CashbackHacker cashbackHacker = new CashbackHacker();
         int actual = cashbackHacker.remain(900);
         int expected = 100;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
 
@@ -20,8 +20,8 @@ class CashbackHackerTest {
     public void shouldShowZero() {
         CashbackHacker cashbackHacker = new CashbackHacker();
         int actual = cashbackHacker.remain(1000);
-        int expected = 1000;
-        assertEquals(actual, expected);
+        int expected = 0;
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -29,7 +29,7 @@ class CashbackHackerTest {
         CashbackHacker cashbackHacker = new CashbackHacker();
         int actual = cashbackHacker.remain(1100);
         int expected = 900;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
 }
